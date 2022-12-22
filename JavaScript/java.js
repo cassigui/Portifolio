58a8206eb604a252d7a863b4ac1b1cd0c09e2bd5
cassigui <107946943+cassigui@users.noreply.github.com>
2022-12-22 10:13:49 -0300

    Update java.js

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -7,8 +7,8 @@
 
  function scrollToIdOnClick (event) {
  	event.preventDefault();
- 	const to = getScrollTopByhref(event.target) -103;
- 	scrollToPosition(to) 
+ 	const to = getScrollTopByhref(event.target);
+ 	scrollToPosition(to * 4 / 4);  
  }
 
  function scrollToPosition(to) {
@@ -21,4 +21,46 @@
   function getScrollTopByhref (element) {
    const id = element.getAttribute('href')
    return document.querySelector(id).offsetTop
+}
+
+/*ANIMAÇÃO*/
+
+ const debounce = function (funcao, esperar, realizar) {
+     let timeout;
+      return function (...args) {
+          const context = this;
+          const later = function() {
+               timeout = null;
+               if (!realizar) funcao.apply (context, args);
+          }
+          const callNow = realizar && !timeout;
+          clearTimeout(timeout)
+          timeout = setTimeout(later, esperar)
+          if (callNow) funcao.apply (context, args);
+      }
+     }
+     
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = 'animate'
+
+function animeScroll () {
+     const windowTop = window.pageYOffset + window.innerHeight ;
+     target.forEach(function(element) {
+          if((windowTop) > element.offsetTop) {
+               element.classList.add(animationClass)
+          } else {
+               element.classList.remove(animationClass)
+          }
+     })
+
+}
+
+ animeScroll ()
+
+if(target.length) {
+window.addEventListener('scroll', debounce(function() {
+     animeScroll();
+     console.log ('kfioaskdiopaskopd')
+}, 200))
 }
